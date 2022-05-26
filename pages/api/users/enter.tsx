967000eeac7cd764b90ba9d4297618f732ff3141
jourmooney <jourmooney@gmail.com>
2022-05-26 13:58:38 +0900

    9.1 Accounts Logic

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -3,8 +3,61 @@ import client from "@libs/client/client";
 import withHandler from "@libs/server/withHandler";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-  console.log(req.body);
-  res.status(200).end();
+  const { phone, email } = req.body;
+  const payload = phone ? { phone: +phone } : { email };
+  let user;
+  user = await client.user.upsert({
+    where: {
+      ...payload,
+      // ...(phone && { phone: +phone }),
+      // ...(email && { email }),
+    },
+    create: {
+      name: "Anonymous",
+      ...payload,
+      // ...(phone ? { phone: +phone } : {}),
+      // ...(email ? { email } : {}),
+    },
+    update: {},
+  });
+  console.log(user);
+  // if (email) {
+  //   user = await client.user.findUnique({
+  //     where: {
+  //       email,
+  //     },
+  //   });
+  //   if (user) console.log("found it");
+  //   if (!user) {
+  //     console.log("Did not find. Will create");
+  //     user = await client.user.create({
+  //       data: {
+  //         name: "Anonymous",
+  //         email,
+  //       },
+  //     });
+  //   }
+  //   console.log(user);
+  // }
+  // if (phone) {
+  //   user = await client.user.findUnique({
+  //     where: {
+  //       phone: +phone,
+  //     },
+  //   });
+  //   if (user) console.log("found it");
+  //   if (!user) {
+  //     console.log("Did not find. Will create");
+  //     user = await client.user.create({
+  //       data: {
+  //         name: "Anonymous",
+  //         phone: +phone,
+  //       },
+  //     });
+  //   }
+  //   console.log(user);
+  // }
+  return res.status(200).end();
 }
 
 export default withHandler("POST", handler);
