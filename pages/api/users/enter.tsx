ae54fb16c8d2db1fb6d581f29791ed0940e37dc8
jourmooney <jourmooney@gmail.com>
2022-05-26 16:13:36 +0900

    9.5 Sending Email

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,8 +1,10 @@
+import mail from "@sendgrid/mail";
 import twilio from "twilio";
 import { NextApiRequest, NextApiResponse } from "next";
 import client from "@libs/client/client";
 import withHandler, { ResponseType } from "@libs/server/withHandler";
 
+mail.setApiKey(process.env.SENDGRID_KEY!);
 const twilioClient = twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
 
 async function handler(
@@ -36,6 +38,15 @@ async function handler(
       body: `Your login token is ${payload}`,
     });
     console.log(message);
+    else if(email) {
+      const email = await mail.send({
+        from: "mail@mail",
+        to: "mail@mail",
+        subject:"Your Carrot Market Verification Email",
+        text: `Your token is${payload}`,
+        html: `<strong>Your token is${payload}</strong>`,
+      })
+    }
   }
   return res.json({
     ok: true,
