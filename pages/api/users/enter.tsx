9f43549479953f5f922ac41378d4fb3ea5733e86
jourmooney <jourmooney@gmail.com>
2022-05-26 16:51:12 +0900

    9.6 Token UI

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,8 +1,8 @@
 import mail from "@sendgrid/mail";
 import twilio from "twilio";
 import { NextApiRequest, NextApiResponse } from "next";
-import client from "@libs/client/client";
 import withHandler, { ResponseType } from "@libs/server/withHandler";
+import client from "@libs/server/client";
 
 mail.setApiKey(process.env.SENDGRID_KEY!);
 const twilioClient = twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
@@ -12,7 +12,7 @@ async function handler(
   res: NextApiResponse<ResponseType>
 ) {
   const { phone, email } = req.body;
-  const user = phone ? { phone: +phone } : { email };
+  const user = phone ? { phone: +phone } : email ? { email } : null;
   if (!user) return res.status(400).json({ ok: false });
   const payload = Math.floor(100000 + Math.random() * 900000) + "";
   const token = await client.token.create({
@@ -32,21 +32,21 @@ async function handler(
     },
   });
   if (phone) {
-    const message = await twilioClient.messages.create({
+    /*  const message = await twilioClient.messages.create({
       messagingServiceSid: process.env.TWILIO_MSID,
       to: process.env.MY_PHONE!,
-      body: `Your login token is ${payload}`,
+      body: `Your login token is ${payload}.`,
     });
-    console.log(message);
-    else if(email) {
-      const email = await mail.send({
-        from: "mail@mail",
-        to: "mail@mail",
-        subject:"Your Carrot Market Verification Email",
-        text: `Your token is${payload}`,
-        html: `<strong>Your token is${payload}</strong>`,
-      })
-    }
+    console.log(message); */
+  } else if (email) {
+    /* const email = await mail.send({
+      from: "nico@nomadcoders.co",
+      to: "nico@nomadcoders.co",
+      subject: "Your Carrot Market Verification Email",
+      text: `Your token is ${payload}`,
+      html: `<strong>Your token is ${payload}</strong>`,
+    });
+    console.log(email); */
   }
   return res.json({
     ok: true,
