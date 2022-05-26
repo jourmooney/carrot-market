2d899f3f879495858b5fc4d268e1fd7128488579
jourmooney <jourmooney@gmail.com>
2022-05-26 11:14:02 +0900

    8.4 withHandler

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,14 +1,10 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import client from "../../../libs/client/client";
+import withHandler from "../../../libs/server/withHandler";
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  if (req.method !== "POST") {
-    res.status(405).end();
-  }
+async function handler(req: NextApiRequest, res: NextApiResponse) {
   console.log(req.body);
-  // res.json({ ok: true });
   res.status(200).end();
 }
+
+export default withHandler("POST", handler);
