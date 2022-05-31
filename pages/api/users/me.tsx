f7add39fedd940864e9fce32209f34ae06412b3a
jourmooney <jourmooney@gmail.com>
2022-05-31 09:40:56 +0900

    9.8 Profile Handler

---
+++ pages/api/users/me.tsx
@@ -0,0 +1,31 @@
+import { withIronSessionApiRoute } from "iron-session/next";
+import { NextApiRequest, NextApiResponse } from "next";
+import withHandler, { ResponseType } from "@libs/server/withHandler";
+import client from "@libs/server/client";
+
+declare module "iron-session" {
+  interface IronSessionData {
+    user?: {
+      id: number;
+    };
+  }
+}
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
+  console.log(req.session.user);
+  const profile = await client.user.findUnique({
+    where: { id: req.session.user?.id },
+  });
+  res.json({
+    ok: true,
+    profile,
+  });
+}
+
+export default withIronSessionApiRoute(withHandler("GET", handler), {
+  cookieName: "carrotsession",
+  password: "12312313245645678978978912312321321432345142143214123421343",
+});
