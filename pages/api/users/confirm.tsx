b7a5739e6f13a7fc18a8584baadd6f5334bf6caf
jourmooney <jourmooney@gmail.com>
2022-05-31 09:40:48 +0900

    9.7 Serverless Sessions

--- pages/api/users/confirm.tsx
+++ pages/api/users/confirm.tsx
@@ -1,13 +1,37 @@
+import { withIronSessionApiRoute } from "iron-session/next";
 import { NextApiRequest, NextApiResponse } from "next";
 import withHandler, { ResponseType } from "@libs/server/withHandler";
+import client from "@libs/server/client";
+
+declare module "iron-session" {
+  interface IronSessionData {
+    user?: {
+      id: number;
+    };
+  }
+}
 
 async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseType>
 ) {
   const { token } = req.body;
-  console.log(token);
+  const exists = await client.token.findUnique({
+    where: {
+      payload: token,
+    },
+    include: { user: true },
+  });
+  if (!exists) return res.status(404).end();
+  console.log(exists);
+  req.session.user = {
+    id: exists.userId,
+  };
+  await req.session.save();
   res.status(200).end();
 }
 
-export default withHandler("POST", handler);
+export default withIronSessionApiRoute(withHandler("POST", handler), {
+  cookieName: "carrotsession",
+  password: "12312313245645678978978912312321321432345142143214123421343",
+});
